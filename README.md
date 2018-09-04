f08f37085ffc5f4cee4b6d5e979af7785574a9b9
Luke Stainlay <lukestainlay@gmail.com>
2018-09-04 16:17:10 +1000

    Update 1616 04/09

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
 # TERM 1 Terminal App Project
 
-## App Name
+## Project Gemstone
+
+
+
+
 
 ![terminalicon.png](/wk3/appname/img/terminalicon.png)
 
 ```
-
-def 1
-2
-3
-blah
-
-```
-trial update 1
-```
-trail update 2
---
+++ gemstone.rb
@@ -0,0 +1,112 @@
+
+
+
+require_relative 'lightforge.rb'
+require_relative 'heartharena.rb'
+
+lightforge = LightForge::LIGHTFORGE
+heartharena = HearthArena::HEARTHARENA
+
+puts """
+
+What is the class you are playing?
+
+[1] Druid
+[2] Hunter
+[3] Mage
+[4] Paladin
+[5] Priest
+[6] Rogue
+[7] Shaman
+[8] Warlock
+[9] Warrior
+
+Please enter a number
+
+"""
+
+numberselection = gets.to_i
+type = ""
+
+case numberselection
+when 1
+    type = "Druid"
+when 2
+    type = "Hunter"
+when 3
+    type = "Mage"
+when 4
+    type = "Paladin"
+when 5
+    type = "Priest"
+when 6
+    type = "Rogue"
+when 7
+    type = "Shaman"
+when 8
+    type = "Warlock"
+when 9
+    type = "Warrior"
+
+end
+
+puts """
+
+You are playing #{type}.
+
+What is your first card?
+
+"""
+
+# how to make first letter upcase ??
+
+cardname = gets.strip.to_s
+
+
+# lightforge value return
+
+i = 0
+y = 0
+
+while i < 1000
+    if lightforge[i][:"Name"] == cardname  
+
+        while y < 10
+            if lightforge[i][:"Scores"][y][:"Hero"] == type
+       
+            lightforgescore = lightforge[i][:"Scores"][y][:"Score"]
+            i = 1000
+            y = 10
+
+            else
+            y += 1
+
+            end
+
+        end
+    
+    else
+        i += 1
+    end
+
+end
+
+
+# #  heartharena value return
+
+heartharenascore = heartharena[type.to_sym][cardname.to_sym]
+
+
+puts """
+
+The Light Forge score is: #{lightforgescore}
+The Hearth Arena score is: #{heartharenascore}
+
+"""
+
+
+
+
+
+
+
